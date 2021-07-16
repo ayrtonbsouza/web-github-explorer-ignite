7846fd31fe29ee5c0d46ac26b62dde550d0416fa
Ayrton Souza <ayrtonsouza@icloud.com>
2021-07-16 16:47:16 -0300

    [:sparkles:] repository list component initial development

---
+++ src/components/RepositoryList.jsx
@@ -0,0 +1,16 @@
+export function RepositoryList() {
+  return (
+    <section className="repository-list">
+      <h1>Lista de Repositórios</h1>
+      <ul>
+        <li>
+          <strong>unform</strong>
+          <p>Forms in React</p>
+          <a href="">
+            Acessar repositórios
+          </a>
+        </li>
+      </ul>
+    </section>
+  )
+}
